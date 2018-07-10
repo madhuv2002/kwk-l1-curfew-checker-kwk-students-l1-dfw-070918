1cc2a8521672b9bb1009cc96eec643b57cc19023
madhuv2002 <madhuv2002@gmail.com>
2018-07-10 23:39:48 +0000

    Done.

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,5 +1,6 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if time >= 11
+    puts "You're in trouble! Better get home quick!"
 end
 
 def curfew_checker(time)
@@ -17,3 +18,4 @@ end
 def platinum_curfew_checker(current_time, curfew_time)
   # code goes here
 end
+
